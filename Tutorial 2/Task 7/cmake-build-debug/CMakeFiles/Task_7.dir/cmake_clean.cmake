7c6f0f8337ce15967197c124625258fef22fd05e
Bailey-1 <44928829+Bailey-1@users.noreply.github.com>
2018-12-03 10:03:41 +0000

    Added Task 7
    Added Task 7. Not finished.

---
+++ Tutorial 2/Task 7/cmake-build-debug/CMakeFiles/Task_7.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Task_7.dir/main.c.obj"
+  "Task_7.pdb"
+  "Task_7.exe"
+  "Task_7.exe.manifest"
+  "Task_7.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Task_7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
